db57e2ac9a4b4bdce3b80450f27b6488b7d6bdaf
unknown <ganeshghate@hotmail.com>
2018-10-11 15:32:24 +0530

    Dynamic Form PO Order Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,67 +1,170 @@
-import { Component, OnInit } from '@angular/core';
-
-import { FormBuilder,FormControl, FormGroup, Validators, ValidationErrors,
-  ValidatorFn, AbstractControl} from '@angular/forms';
+import { Component, ViewChild } from "@angular/core";
+import { Validators } from "@angular/forms";
+import { FieldConfig } from "./field.interface";
+import { DynamicFormComponent } from "./components/dynamic-form/dynamic-form.component";
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit {
-
-  POForm: FormGroup;
-  manufacturerName = new FormControl();
-  orderid = new FormControl();
-  poNumber = new FormControl();
-  createDate = new FormControl();
-  issueDate = new FormControl();
-  supplierName = new FormControl();
-  password = new FormControl();
-  notification = new FormControl();
-  datepickerone = new FormControl();
-  chk = new FormControl();
-  datepickertwochk = new FormControl();
-
-  datetwochk = false; 
-  submitted = false;
-
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-      this.POForm = this.formBuilder.group({
-//            firstName: ['', [Validators.required, this.PatternValidator()]],
-          manufacturerName: ['', [Validators.required, Validators.pattern( /^[a-zA-Z ]*$/)]],
-          orderid: ['', [Validators.required]],
-          poNumber: ['', [Validators.required]],
-          createDate: ['', Validators.required],
-          issueDate: ['', Validators.required],
-          supplierName: ['', [Validators.required, Validators.pattern( /^[a-zA-Z ]*$/)]],
-          email: ['', [Validators.required, Validators.email]],
-          password: ['', [Validators.required, Validators.minLength(6)]],
-          notification: [''],
-          description: [''],
-          datepickerone: ['', Validators.required],
-          chk: [''],
-          datepickertwochk: ['']
-      });
-  }
-
-  get f() { return this.POForm.controls; }
-
-  onSubmit() {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.POForm.invalid) {
-        return;
+export class AppComponent {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
+  regConfig: FieldConfig[] = [
+    {
+      collections: "no",
+      type: "input",
+      label: "Manufacturer Name",
+      inputType: "text",
+      name: "manufacturerName",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Name Required"
+        },
+        {
+          name: "pattern",
+          validator: Validators.pattern("^[a-zA-Z]+$"),
+          message: "Accept only text"
+        }
+      ]
+    },
+    {
+      type: "input",
+      label: "Order Id",
+      inputType: "number",
+      name: "orderid",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "PO No",
+      inputType: "number",
+      name: "poNumber",
+      collections: "no",
+    },
+    {
+      type: "date",
+      label: "Date of Creation",
+      name: "createDate",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Date of Creation Required"
+        }
+      ],
+      collections: "no",
+    },
+    {
+      type: "date",
+      label: "Date of Issue",
+      name: "issueDate",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Date of Issue Required"
+        }
+      ],
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "Supplier Name",
+      inputType: "text",
+      name: "supplierName",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "Supplier Address",
+      inputType: "text",
+      name: "supplierAddrs",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "Supplier Delivery Address",
+      inputType: "text",
+      name: "delivryAddrs",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "Supplier Billing Address",
+      inputType: "text",
+      name: "billingAddrs",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "product",
+      inputType: "text",
+      name: "product",
+      collections: "yes",
+    },
+    {
+      type: "input",
+      label: "Grand Total",
+      inputType: "number",
+      name: "grndTotal",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "Advance Payment",
+      inputType: "number",
+      name: "advPaymnt",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "Net Payable",
+      inputType: "number",
+      name: "netPayable",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "Amount in Words",
+      inputType: "text",
+      name: "amtInWords",
+      collections: "no",
+    },
+    {
+      type: "input",
+      label: "Status",
+      inputType: "text",
+      name: "status",
+      collections: "no",
+    },
+    {
+      collections: "no",
+      type: "button",
+      label: "Save"
     }
+  ];
 
-    console.log(this.POForm);
-    console.log(this.POForm.value.datepickerone.toLocaleDateString());
+  submit(value: any) {
+    var localeCreatedDate = value.createDate.toLocaleTimeString();
+    var getdate = value.createDate.getDate();
+    var toLocaleCreatedString = value.createDate.toLocaleString();
+    var toLocaleIssueString = value.issueDate.toLocaleString();
+    var toLocaleDateString = value.createDate.toLocaleDateString();
+    console.log(localeCreatedDate);
+    console.log(getdate);
+    console.log(toLocaleCreatedString);
+    console.log(toLocaleDateString);
+    // var localeCreatedDateString = localeCreatedDate.toLocaleString();
+    // value.createDate = localeCreatedDateString;
+    // var localeIssueDate = value.issueDate.toDate();
+    // var localeIssueDateString = localeIssueDate.toLocaleString();
+    // value.issueDate = localeIssueDateString;
+    value.createDate = toLocaleCreatedString;
+    value.issueDate = toLocaleIssueString;
+    console.log(value);
+  }
 }
-
-}
-
-
---
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,180 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output
+} from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormArray
+} from "@angular/forms";
+import { FieldConfig, Validator } from "../../field.interface";
+
+export class Adresse {
+  product: string;
+  description: string;
+  unit: string;
+  qty: number;
+  unitPrice: number;
+  totalPrice: number;
+  taxCode: string;
+}
+
+
+@Component({
+  exportAs: "dynamicForm",
+  selector: "dynamic-form",
+  template: `
+  <form class="dynamic-form" [formGroup]="form" (submit)="onSubmit($event)">
+    <ng-container *ngFor="let field of fields;" >
+      <div *ngIf="field.collections === 'yes'">
+        <div formArrayName="ProductDetails">
+          <div *ngFor="let itemrow of adresses.controls; let i=index" >
+            <div [formGroupName]="i">
+              <input type="text" name="prodcut" formControlName="product">
+              <input type="text" name="description" formControlName="description"> 
+              <input type="text" name="unit" formControlName="unit"> 
+              <input type="number" name="qty" formControlName="qty"> 
+              <input type="number" name="unitPrice" formControlName="unitPrice"> 
+              <input type="number" name="totalPrice" formControlName="totalPrice"> 
+              <input type="text" name="taxCode" formControlName="taxCode"> 
+            </div>
+            <button type="button"  (click)="add();">Add More PO Details</button>
+            <button type="button"  (click)="removeAddress(i);">Delete PO Details</button>
+          </div>
+        </div>
+      </div>
+      <div *ngIf="field.collections === 'no'">
+        <div dynamicField [field]="field" [group]="form">
+        </div>
+      </div>
+    </ng-container>
+  </form>
+  `,
+  styles: []
+})
+export class DynamicFormComponent implements OnInit {
+  @Input() fields: FieldConfig[] = [];
+
+  @Output() submit: EventEmitter<any> = new EventEmitter<any>();
+
+  form: FormGroup;
+  manufacturerName: FormControl;
+  orderid: FormControl;
+  poNumber: FormControl;
+  createDate: FormControl;
+  issueDate: FormControl;
+  supplierName: FormControl;
+  supplierAddrs: FormControl;
+  delivryAddrs: FormControl;
+  billingAddrs: FormControl;
+  ProductDetails: FormArray;
+  product: FormControl;
+  description: FormControl;
+  unit: FormControl;
+  qty: FormControl;
+  unitPrice: FormControl;
+  totalPrice: FormControl;
+  taxCode: FormControl;
+  grndTotal: FormControl;
+  advPaymnt: FormControl;
+  netPayable: FormControl;
+  amtInWords: FormControl;
+  status: FormControl;
+  adresse: Adresse = new Adresse();
+  get value() {
+    return this.form.value;
+  }
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.createControl(this.listAddres[0]);
+    //this.form.setControl('ProductDetails', this.fb.array((this.listAddres || []).map((x) => this.fb.group(x))));
+  }
+
+
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (this.form.valid) {
+      this.submit.emit(this.form.value);
+    } else {
+      this.validateAllFormFields(this.form);
+    }
+  }
+
+  createControl(adresse: Adresse) {
+    this.form = this.fb.group({
+      manufacturerName:new FormControl(),
+      orderid:new FormControl(),
+      poNumber:new FormControl(),
+      createDate:new FormControl(),
+      issueDate:new FormControl(),
+      supplierName:new FormControl(),
+      supplierAddrs:new FormControl(),
+      delivryAddrs:new FormControl(),
+      billingAddrs:new FormControl(),
+      ProductDetails: this.fb.array([this.createForms(adresse)]),    
+      grndTotal:new FormControl(),
+      advPaymnt:new FormControl(),
+      netPayable:new FormControl(),
+      amtInWords:new FormControl(),
+      status:new FormControl(),
+    });
+    return this.form;
+  }
+
+  
+  createForms(adresse): FormGroup {
+    return this.fb.group({
+           product: new FormControl(adresse.product),
+           description: new FormControl(adresse.description ),
+           unit: new FormControl(adresse.unit ),
+           qty: new FormControl(adresse.qty ),
+           unitPrice: new FormControl(adresse.unitPrice ),
+           totalPrice: new FormControl(adresse.totalPrice ),
+           taxCode: new FormControl(adresse.taxCode ),
+    })
+  }
+  listAddres: Adresse[] = [ 
+      {product:'', description:'', unit:'', qty:0, unitPrice:0, totalPrice:0, taxCode:'' }
+  ];
+
+  get adresses() {
+    return this.form.get('ProductDetails') as FormArray;
+  }
+
+  add() {
+     this.adresses.push(this.createForms(this.adresse));
+  }
+
+  removeAddress(i: number) {  
+    const control = <FormArray>this.form.controls['ProductDetails'];
+    control.removeAt(i);
+  }
+
+
+  bindValidations(validations: any) {
+    if (validations.length > 0) {
+      const validList = [];
+      validations.forEach(valid => {
+        validList.push(valid.validator);
+      });
+      return Validators.compose(validList);
+    }
+    return null;
+  }
+
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      control.markAsTouched({ onlySelf: true });
+    });
+  }
+}
